cba68d544cb3f555c48d4c339bf17738b3dcb446
312k <55316963+312k@users.noreply.github.com>
2022-09-23 21:39:12 +0900

    フォントと音楽ファイルの依存関係の追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 ﻿#ifdef __GNUC__
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
 #endif
 #ifdef _MSC_VER
 #include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
 #endif
 int main(int argc, char** argv) {
 	SDL_Window* window;
@@ -12,10 +19,19 @@ int main(int argc, char** argv) {
 		SDL_Quit();
 		return 1;
 	}
-	if ((window = SDL_CreateWindow("Tajpado", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_RESIZABLE)) == NULL) {
+	// Windowの作成、TTFの初期化に失敗したとき
+	if ((window = SDL_CreateWindow("Tajpado", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_RESIZABLE)) == NULL || TTF_Init() == -1) {
 		SDL_Quit();
 		return 1;
 	}
+	TTF_Font* font = NULL;
+
+	SDL_Rect time_rect{
+		time_rect.x = 520,
+		time_rect.y = 260,
+		time_rect.w = 300,
+		time_rect.h = 90
+	};
 	SDL_SetWindowTitle(window, "Tajpado");
 	//レンダラー
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
@@ -24,7 +40,11 @@ int main(int argc, char** argv) {
 	//背景をクリア
 	SDL_RenderClear(renderer);
 	//フォントを読み込む
-
+	// TODO:プロジェクト内にフォントを置く
+	if ((font = TTF_OpenFont("C:/Users/mazok/AppData/Local/Microsoft/Windows/Fonts/HackGen35ConsoleNFJ-Regular.ttf", 100)) == NULL) {
+		SDL_Quit();
+		return 1;
+	}
 	while (1)
 	{
 		//閉じるボタンで閉じれるようにする
@@ -32,6 +52,10 @@ int main(int argc, char** argv) {
 		if (exit.type == SDL_QUIT) {
 			break;
 		}
+		SDL_RenderCopy(renderer,
+			SDL_CreateTextureFromSurface(renderer,
+				TTF_RenderUTF8_Blended(font, reinterpret_cast<const char*>(u8"ねこちゃん"), SDL_Color{ 0,120, 120, 120 })),
+			NULL, &time_rect);
 		// 画面に反映させる
 		SDL_RenderPresent(renderer);
 	}
--- src/xmake.lua
+++ src/xmake.lua
@@ -1,9 +1,9 @@
 add_rules("mode.debug", "mode.release")
 
-add_requires("libsdl")
+add_requires("libsdl","libsdl_image","libsdl_mixer","libsdl_ttf")
 
 target("main")
     set_kind("binary")
     add_files("main.cpp")
-    add_packages("libsdl")
+    add_packages("libsdl","libsdl_image","libsdl_mixer","libsdl_ttf")
     add_links("sdl2")
