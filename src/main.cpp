f7f0ca5bd6d60fecf6800c70ca69a4034a518d24
312k <55316963+312k@users.noreply.github.com>
2022-09-24 21:02:13 +0900

    フォントファイルの追加

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,17 @@
 #endif
 int main(int argc, char** argv) {
 	SDL_Window* window;
+	// 終了イベント
 	SDL_Event exit;
+	// フォント
+	TTF_Font* font = NULL;
+	// 文字列描画構造体
+	SDL_Rect time_rect{
+		time_rect.x = 520,
+		time_rect.y = 260,
+		time_rect.w = 300,
+		time_rect.h = 90
+	};
 	// SDL2の初期化
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 		SDL_Quit();
@@ -24,14 +34,7 @@ int main(int argc, char** argv) {
 		SDL_Quit();
 		return 1;
 	}
-	TTF_Font* font = NULL;
 
-	SDL_Rect time_rect{
-		time_rect.x = 520,
-		time_rect.y = 260,
-		time_rect.w = 300,
-		time_rect.h = 90
-	};
 	SDL_SetWindowTitle(window, "Tajpado");
 	//レンダラー
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
@@ -39,33 +42,25 @@ int main(int argc, char** argv) {
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	//背景をクリア
 	SDL_RenderClear(renderer);
-	//フォントを読み込む
-	// TODO:プロジェクト内にフォントを置く
-#ifdef __GNUC__
-	if ((font = TTF_OpenFont("/usr/share/fonts/TTF/HackGen-Regular.ttf", 100)) == NULL) {
-		SDL_Quit();
-	return 1;
-	}
-#endif
-#ifdef _MSC_VER
-	if ((font = TTF_OpenFont("C:/Users/mazok/AppData/Local/Microsoft/Windows/Fonts/HackGen35ConsoleNFJ-Regular.ttf", 100)) == NULL) {
+	if ((font = TTF_OpenFont("HackGen-Regular.ttf", 50)) == NULL) {
 		SDL_Quit();
 		return 1;
 	}
-#endif
-		SDL_RenderCopy(renderer,
-			SDL_CreateTextureFromSurface(renderer,
-				TTF_RenderUTF8_Blended(font, reinterpret_cast<const char*>(u8"ねこちゃん"), SDL_Color{ 0,120, 120, 120 })),
-			NULL, &time_rect);
-		// 画面に反映させる
-		SDL_RenderPresent(renderer);
 	while (1)
 	{
+		//背景をクリア
+		SDL_RenderClear(renderer);
 		//閉じるボタンで閉じれるようにする
 		SDL_PollEvent(&exit);
 		if (exit.type == SDL_QUIT) {
 			break;
 		}
+		SDL_RenderCopy(renderer,
+		SDL_CreateTextureFromSurface(renderer,
+			TTF_RenderUTF8_Blended(font,"ねこちゃん", SDL_Color{ 0,120, 120, 120 })),NULL, &time_rect);
+		// 画面に反映させる
+		SDL_RenderPresent(renderer);
+		SDL_Delay(10);
 	}
 	SDL_DestroyWindow(window);
 	SDL_Quit();
