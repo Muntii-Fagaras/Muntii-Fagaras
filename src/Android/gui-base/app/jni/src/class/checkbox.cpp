879a4b6e52eddfd21ed1be1031af9dd16c0ca1f2
312k <55316963+312k@users.noreply.github.com>
2022-10-09 01:03:22 +0900

    Android Projectの更新

---
+++ src/Android/gui-base/app/jni/src/class/checkbox.cpp
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "checkbox.hpp"
+#include "mouse.hpp"
+checkbox::checkbox(std::vector<std::string> image_path, SDL_Renderer* renderer, int x, int y,bool mouse_state)
+{
+	checkbox_state = mouse_state;
+	load(image_path.at(checkbox_state), renderer, x, y);
+
+}
+
+checkbox::~checkbox()
+{
+	SDL_FreeSurface(image_surface);
+	SDL_DestroyTexture(texture);
+	texture = nullptr;	//とりあえずnullptrを入れとく
+}
